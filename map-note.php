86009e5b7efa153b74a14e4fc585e8533f24e321
changwoo <ep6tri@hotmail.com>
2024-11-10 18:21:13 +0900

    샘플 코드 커밋

---
+++ map-note.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Map Note
+ */
+
+use JetBrains\PhpStorm\NoReturn;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! function_exists( 'map_note_add_rewrite_rules' ) ) {
+    function map_note_add_rewrite_rules(): void {
+        add_rewrite_rule( 'map-note/(.*)$', 'wp-content/plugins/map-note/dist/$1', 'top' );
+    }
+    add_action( 'init', 'map_note_add_rewrite_rules' );
+}
+
+if ( ! function_exists( 'map_note_query_vars' ) ) {
+    function map_note_query_vars( array $vars ): array {
+        return [...$vars, 'map-note'];
+    }
+    add_filter( 'query_vars', 'map_note_query_vars' );
+}
+
+if ( ! function_exists( 'map_note_template_redirect' ) ) {
+    function map_note_template_redirect(): void {
+        if ( 'map-note' == get_query_var( 'map-note' ) ) {
+            echo 'okay!';
+            exit;
+        }
+    }
+    add_action( 'template_redirect', 'map_note_template_redirect' );
+}
